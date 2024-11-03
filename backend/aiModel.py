75bcbd7fc127931d13470d11fed323ed56b52bb0
Andry Arthur <andry1arthur@gmail.com>
2024-11-03 03:47:54 -0500

    Changing aiModel to import trained model

--- backend/aiModel.py
+++ backend/aiModel.py
@@ -2,8 +2,34 @@ import tensorflow as tf
 from PIL import image
 import os
 
+LABELS = [
+    'Acne and Rosacea Photos',
+    'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions',
+    'Atopic Dermatitis Photos',
+    'Bullous Disease Photos',
+    'Cellulitis Impetigo and other Bacterial Infections',
+    'Eczema Photos',
+    'Exanthems and Drug Eruptions',
+    'Hair Loss Photos Alopecia and other Hair Diseases',
+    'Herpes HPV and other STDs Photos',
+    'Light Diseases and Disorders of Pigmentation',
+    'Lupus and other Connective Tissue diseases',
+    'Melanoma Skin Cancer Nevi and Moles',
+    'Nail Fungus and other Nail Disease',
+    'Poison Ivy Photos and other Contact Dermatitis',
+    'Psoriasis pictures Lichen Planus and related diseases',
+    'Scabies Lyme Disease and other Infestations and Bites',
+    'Seborrheic Keratoses and other Benign Tumors',
+    'Systemic Disease',
+    'Tinea Ringworm Candidiasis and other Fungal Infections',
+    'Urticaria Hives',
+    'Vascular Tumors',
+    'Vasculitis Photos',
+    'Warts Molluscum and other Viral Infections'
+]
+
 # Load the TensorFlow model (update the path to where your trained model is saved)
-model = tf.keras.models.load_model("path/to/your_skin_condition_model.h5")
+model = tf.keras.models.load_model("./model/tf_model.keras")
 
 def skin_analysis(filename: str):
     # Path where images are stored on the server
@@ -18,7 +44,7 @@ def skin_analysis(filename: str):
 
         # Use the model to predict the condition, directly returning a string
         prediction = model.predict(img_array)
-        condition = prediction[0]  # Assuming the model returns a single string label
+        condition = LABELS[prediction.argmax()]
 
         return {"condition": condition}
     
